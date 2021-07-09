ec3112ebdb85fc7c5752c33dec42b126a1cb8e6a
vsreyes <virgilio.s.reyes@gmail.com>
2021-07-09 23:42:56 +0200

    Validation setup

---
+++ app.js
@@ -0,0 +1,11 @@
+// Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('.#guess-btn'),
